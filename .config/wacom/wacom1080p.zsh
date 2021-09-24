92e347a775c337a72bb7ff67f4f64fa6adbebea4
Konsta Kuosmanen <konstamskuosmanen@gmail.com>
2021-09-24 14:56:57 +0200

    added wacom support (1080p only)

--- .config/wacom/wacom1080p.zsh
+++ .config/wacom/wacom1080p.zsh
@@ -1,2 +1,2 @@
-xsetwacom set 16 MapToOutput 1920x1080+1920+0
-xsetwacom set 16 Area 0 0 15200 8550
+xsetwacom set `wacomid stylus` MapToOutput 1920x1080+1920+0
+xsetwacom set `wacomid stylus` Area 0 0 15200 8550
