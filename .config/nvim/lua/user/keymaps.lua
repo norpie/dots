1234e9e65b0afad86931e7bcb1b160b034e3032d
Konsta Kuosmanen <konstamskuosmanen@gmail.com>
2022-02-23 16:52:58 +0100

    fixed split resizing

--- .config/nvim/lua/user/keymaps.lua
+++ .config/nvim/lua/user/keymaps.lua
@@ -44,7 +44,7 @@ map('n', '<C-l>', '<C-w>l')
 
 map('n', '<C-Up>', ':resize +2<CR>')
 map('n', '<C-Down>', ':resize -2<CR>')
-map('n', '<C-Left>', ':verical resize -2<CR>')
+map('n', '<C-Left>', ':vertical resize -2<CR>')
 map('n', '<C-Right>', ':vertical resize +2<CR>')
 
 map('n', '<Leader>sh', ':split<CR>')
