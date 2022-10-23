aa43b96797bb49cb3c0603d497806763be95b1cd
Konsta Kuosmanen <konstamskuosmanen@gmail.com>
2022-10-24 00:07:17 +0200

    even more nvim stuff

--- .config/nvim/lua/config/theme.lua
+++ .config/nvim/lua/config/theme.lua
@@ -5,6 +5,11 @@ function M.setup()
         flavour = "macchiato" -- mocha, macchiato, frappe, latte
     }
     vim.api.nvim_command "colorscheme catppuccin-macchiato"
+    local highlight = require('util').highlight
+
+    highlight(0, 'VertSplit', {
+        fg = '#202233'
+    })
 end
 
 return M
