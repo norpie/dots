ba7d52e0221ea07805f72beb3a93bbef5601f386
Konsta Kuosmanen <konstamskuosmanen@gmail.com>
2022-11-24 21:46:18 +0100

    corrected field in json kraken api

--- .local/bin/python/kraken-api.py
+++ .local/bin/python/kraken-api.py
@@ -1,3 +1,3 @@
 import requests
 
-print(requests.get("https://api.kraken.com/0/public/Ticker?pair=XBTEUR").json()['result']['XXBTZEUR']['b'][0])
+print(requests.get("https://api.kraken.com/0/public/Ticker?pair=XBTEUR").json()['result']['XXBTZEUR']['a'][0])
