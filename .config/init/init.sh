6108886a2edb78c48a08069d43a4cb2555d9f1e5
Konsta Kuosmanen <konstamskuosmanen@gmail.com>
2022-11-28 14:01:52 +0100

    output-fixer -> timed-fixer

--- .config/init/init.sh
+++ .config/init/init.sh
@@ -5,7 +5,7 @@ source ~/.config/zsh/.zshrc
 xorg-state-load
 numlockx on
 xroot-status-restart
-start output-fixer
+start timed-fixer
 escapecaps
 xwallpaper --zoom ~/.config/wallpapers/wallpaper.png
 sleep 5 && xset s off && xset -dpms
