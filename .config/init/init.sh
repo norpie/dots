d9fdd0c9c8a166bb1910ac2283b8c53b6ecd5fca
Konsta Kuosmanen <konstamskuosmanen@gmail.com>
2023-02-01 19:18:13 +0100

    xresources finally work

--- .config/init/init.sh
+++ .config/init/init.sh
@@ -5,7 +5,7 @@ source ~/.config/zsh/.zshrc
 xorg-state-load
 numlockx on
 xroot-status-restart
+ssh-add-defaults
 start timed-fixer
 xwallpaper --zoom ~/.config/wallpapers/wallpaper.png
-xresources-reload
 sleep 5 && xset s off && xset -dpms
