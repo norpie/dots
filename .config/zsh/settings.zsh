d30d3ad14ce9c12985c9664b236d72993e7aaf00
Konsta Kuosmanen <konstamskuosmanen@gmail.com>
2023-02-02 11:51:42 +0100

    history

--- .config/zsh/settings.zsh
+++ .config/zsh/settings.zsh
@@ -13,8 +13,13 @@ bindkey "$terminfo[kcud1]" history-substring-search-down
 autoload -U compinit
 setopt prompt_subst
 setopt PROMPT_SUBST
-setopt histignoredups
 setopt inc_append_history share_history
+setopt HIST_EXPIRE_DUPS_FIRST
+setopt HIST_IGNORE_DUPS
+setopt HIST_IGNORE_ALL_DUPS
+setopt HIST_IGNORE_SPACE
+setopt HIST_FIND_NO_DUPS
+setopt HIST_SAVE_NO_DUPS
 compinit
 _comp_options+=(globdots)
 fast-theme clean &>/dev/null
