9ad13149094db4a08067a3618446c4c87ff1329b
norpie (Konsta Kuosmanen) <contact@norpie.dev>
2025-03-08 10:22:50 +0100

    feat: added initial install script

---
+++ install.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+# This script clones my dotfiles and installs them
+
+REPO="https://github.com/norpie/dots"
+HOME_DIR=$(eval echo ~${SUDO_USER})
+HOMES_DIR=$(dirname $HOME_DIR)
+
+cd $HOMES_DIR &&
+sudo rm -rf $HOME_DIR &&
+sudo git clone $REPO $HOME_DIR &&
+sudo chown -R $SUDO_USER $HOME_DIR &&
+cd $HOME_DIR &&
+mv .git .dots
