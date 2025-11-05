05e55ff0dd6dbcb231bca31376ace8cfb8ef00d8
norpie (Konsta Kuosmanen) <contact@norpie.dev>
2025-11-05 19:59:15 +0100

    feat: fish stuff

--- .config/hypr/init.sh
+++ .config/hypr/init.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/env zsh
+#!/usr/bin/env bash
 
 # Java applications compatibility
 export _JAVA_AWT_WM_NONREPARENTING=1
@@ -13,13 +13,11 @@ ssh-add-defaults
 echo $SSH_AUTH_SOCK > ~/.cache/ssh_auth_sock
 echo $SSH_AGENT_PID > ~/.cache/ssh_agent_pid
 
-source ~/.config/zsh/.zshrc
+# Ensure PATH includes .local/bin for reload command
+export PATH="$HOME/.local/bin:$PATH"
 
 # Cache Wayland display for scripts (equivalent to X11's DISPLAY cache)
 echo $WAYLAND_DISPLAY > ~/.cache/wayland_display
 
 # Cache Hyprland instance signature for hyprctl commands
 echo $HYPRLAND_INSTANCE_SIGNATURE > ~/.cache/hyprland_instance
-
-reload hyprland
-reload waybar
